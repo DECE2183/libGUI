8d4eb7425d116bc1bc8ddea2f2e87f2ce849d515
DECE <tim.korolik@gmail.com>
2021-11-05 14:48:28 +0300

    Refactor

--- src/Fonts/DefaultFont.h
+++
@@ -1,6 +0,0 @@
-#pragma once
-#include "JetBrainsMono8x14.h"
-#include "Symbols16x16.h"
-
-static const GFont &DefaultFont = JetBrainsMono8x14;
-static const GFont &DefaultSymbolFont = Symbols16x16;
--- src/libGUI.h
+++
@@ -1,7 +0,0 @@
-#pragma once
-
-#include <Elements/Section.h>
-#include <Elements/Label.h>
-#include <Elements/Window.h>
-#include <Elements/Button.h>
-#include <Elements/Range.h>
